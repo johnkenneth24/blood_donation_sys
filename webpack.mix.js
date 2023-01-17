85b3bf6ddceb383306be5227c944651b210f658e
Mike Stephen Gollena <55428756+DJmistMAGO@users.noreply.github.com>
2023-01-17 16:32:45 +0800

    add basic landing page and login

---
+++ webpack.mix.js
@@ -0,0 +1,23 @@
+const mix = require("laravel-mix");
+const fs = require("fs");
+
+let getFiles = function (dir) {
+    return fs.readdirSync(dir).filter((file) => {
+        return fs.statSync(`${dir}/${file}`).isFile();
+    });
+};
+
+// TODO add the directories here relative to resources/js/
+var dirs = [
+    "", // for files under js
+];
+var resPath = "resources/js/";
+var pubPath = "public/js/";
+
+dirs.forEach(function (item, index) {
+    getFiles(resPath + item).forEach(function (filepath) {
+        mix.js(resPath + item + filepath, pubPath + item).version();
+    });
+});
+
+mix.disableNotifications();
