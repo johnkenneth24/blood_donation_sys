99b57f42991dedf048d55956e8ce13e6f963a223
jkenneth24 <kengerero@yahoo.com>
2023-01-17 22:00:53 +0800

    landing page

---
+++ resources/views/modules/landingpage/contact/contact.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title')
+    Contact
+@endsection
+
+@section('content') 
+ <section id="about" class="container-fluid d-flex justify-content-center align-items-center">
+              <div class="row">
+                  contact
+                 </div>
+              </div> 
+            </section>
+@endsection
---
+++ resources/views/modules/landingpage/home/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('title')
+    Welcome to BDMS!
+@endsection
+
+@section('content') 
+ 
+ <section id="landingHome" class="container-fluid d-flex justify-content-center align-items-center">
+    <div class="row">
+        <div class="text-landing">
+            <h1 class="text-center">BLOOD <br> DONATION <br> MANAGEMENT <br> SYSTEM</h1>
+            <blockquote class="text-center">"Let's Come to Donate Blood"</blockquote>
+        </div>
+    </div>
+</section>
+
+@endsection
